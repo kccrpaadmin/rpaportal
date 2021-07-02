57c1c4ae2558d49326d09acd1b67ea16bf491c6a
Administrator <psmpsm9197@naver.com>
2021-07-02 14:21:30 +0900

    2021.07.02 차트 라이브러리 설치

---
+++ RpaPortal/src/main/webapp/resources/js/library/rMateChartH5Web_v6.0/LicenseKey/rMateChartH5License.js
@@ -0,0 +1,12 @@
+// -----------------------------------------------------------------------------
+// rMate Chart H5 License Key 
+//
+// Product Name : rMate Chart for HTML5 v6.0
+// License Type : Enterprise License
+// Product No : F63E-FD34-97F7-E7E5
+// Customer : KCC건설(RPA 시스템 구축 )
+// Authenticated server Info : the number of server= 2, server's IP= 70.71.14.123(OP),70.71.14.119(DEV), Access Domain= rpa.kccworld.net, rpadev.kccworld.net
+// Expiration date : unlimited
+//
+var rMateChartH5License = "782933115053969441a3a29e8658fa366dd9d7ffd5f7514a91bcd58e48f8be01:6400330b343a2a4520204c743a65466e362e3364456c2d72466f4477336334632d6b392e3776466537642d6145703772452c35742065506e422e3a64486c44724f6f20774c6356633a6b322e2e6130702072502c56393a314331352e2d3445314e2e2d3136372e2e30302037452c4c333a326631202e43343a31322e30313237312e30303737303a3148";
+// -----------------------------------------------------------------------------
