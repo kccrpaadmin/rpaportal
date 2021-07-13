dbea386bed27429a4be1b73486fae1ce24a348e2
Administrator <suyeon26@kccworld.net>
2021-07-14 08:54:44 +0900

    2021.07.14 vendor 파일 삭제

--- RpaPortal/src/main/java/com/kcc/biz/service/IBotVendorService.java
+++
@@ -1,9 +0,0 @@
-package com.kcc.biz.service;
-
-import java.util.List;
-
-import com.kcc.biz.model.BotVendorVO;
-
-public interface IBotVendorService {	
-	List<BotVendorVO> listBotVendor(BotVendorVO vo) throws Exception;
-}
