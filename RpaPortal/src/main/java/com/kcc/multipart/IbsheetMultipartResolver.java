aacb21232b350720623156188d937ffe731b64ca
Administrator <psmpsm9197@naver.com>
2021-09-24 16:00:26 +0900

    2021.09.24 아이비시트 엑셀 업로드 수정

--- RpaPortal/src/main/java/com/kcc/multipart/IbsheetMultipartResolver.java
+++ RpaPortal/src/main/java/com/kcc/multipart/IbsheetMultipartResolver.java
@@ -2,12 +2,17 @@ package com.kcc.multipart;
 
 import javax.servlet.http.HttpServletRequest;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
-public class IbsheetMultipartResolver extends CommonsMultipartResolver {
+import com.kcc.controller.common.AccountController;
 
+public class IbsheetMultipartResolver extends CommonsMultipartResolver {
+	private static final Logger logger = LoggerFactory.getLogger(IbsheetMultipartResolver.class);
+	
 	public boolean isMultipart(HttpServletRequest request) {
-		System.out.println("IbsheetMultipartResolver #### " + ServletFileUpload.isMultipartContent(request));
+		logger.info("IbsheetMultipartResolver #### " + ServletFileUpload.isMultipartContent(request));
 
 		//ibsheet를 통한 호출, 다운로드나 업로드시 SheetWork 라는 파라미터를 보냄
 		String sheetwork = request.getParameter("SheetWork");
